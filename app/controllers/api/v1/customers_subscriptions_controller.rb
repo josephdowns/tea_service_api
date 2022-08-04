3b091cd19f8add819522e56efec725c4a8afdf8f
Joseph Downs <jos.w.downs@gmail.com>
2022-08-03 22:31:51 -0600

    Feat: cancel sub

--- app/controllers/api/v1/customers_subscriptions_controller.rb
+++ app/controllers/api/v1/customers_subscriptions_controller.rb
@@ -7,6 +7,13 @@ class Api::V1::CustomersSubscriptionsController < ApplicationController
     end
   end
 
+  def update
+    customer = Customer.find(params[:customer_id])
+    cs = CustomersSubscription.find_by(customer_id: params[:customer_id], subscription_id: params[:subscription_id])
+    cs.update_attribute(:status, 1)
+    render json: CustomerSerializer.show(customer)
+  end
+
   def customers_subscriptions_params
     params.permit(:customer_id, :subscription_id)
   end
--- spec/requests/api/v1/customers_subscriptions/customers_subscriptions_request_spec.rb
+++ spec/requests/api/v1/customers_subscriptions/customers_subscriptions_request_spec.rb
@@ -7,16 +7,8 @@ RSpec.describe "customers_subscriptions request", type: :request do
   end
   context "customer can create a subscription" do
     it "creates a relationship" do
-      headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-        }
-    
-      body = {
-      'customer_id': @celes.id,
-      'subscription_id': @tea.id
-      }
-      post "/api/v1/subscriptions", headers: headers, params: JSON.generate(body)
+  
+      post "/api/v1/customers/#{@celes.id}/subscriptions/#{@tea.id}/new"
 
       celes = Customer.last
 
@@ -29,6 +21,15 @@ RSpec.describe "customers_subscriptions request", type: :request do
     it "cancels the subscription" do
       terra = Customer.create!({first_name: "Terra", last_name: "Branford", email: "terra@ffvi.com", address: "1234 Narshe", subscription: true}) 
       CustomersSubscription.create!({customer_id: terra.id, subscription_id: @tea.id, status: 0 })
+
+      patch "/api/v1/customers/#{terra.id}/subscriptions/#{@tea.id}/edit"
+
+      new_cs = CustomersSubscription.last
+      show = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(new_cs.status).to eq("canceled")
+      expect(show[:subscriptions][:active].count).to be(0)
+      expect(show[:subscriptions][:canceled].count).to be(1)
       
     end
   end
