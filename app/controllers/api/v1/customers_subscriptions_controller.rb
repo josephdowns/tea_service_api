e39ffa71d858ff8d15e3c6ed9009d674e608eac1
Joseph Downs <jos.w.downs@gmail.com>
2022-08-05 12:42:21 -0600

    Add readme
    Create CSSerializer

--- app/controllers/api/v1/customers_subscriptions_controller.rb
+++ app/controllers/api/v1/customers_subscriptions_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::CustomersSubscriptionsController < ApplicationController
   def create
     cs = CustomersSubscription.create(customers_subscriptions_params)
-    render json: {message: 'Successfully subscribed'}, status: 201
+    render json: CustomersSubscriptionSerializer.new(cs), status: 201
   end
 
   def update
     customer = Customer.find(params[:customer_id])
     cs = CustomersSubscription.find_by(customer_id: params[:customer_id], subscription_id: params[:subscription_id])
     cs.update_attribute(:status, 1)
-    render json: {message: 'Successfully unsubscribed'}
+    render json: CustomersSubscriptionSerializer.cancel(cs)
   end
 
   def customers_subscriptions_params
